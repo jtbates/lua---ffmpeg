a09204dbd40ac8179984a674d7488a71b5c4c802
JT Bates <jtbates@gmail.com>
2012-04-01 19:40:36 -0400

    Sets current frame at init

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ do
       -- cleanup path
       self.path = self.path:gsub('^~',os.getenv('HOME'))
 
+      -- set current frame pointer to 1
+      self.current = 1
+
       -- is data provided ?
       if self.tensor then
          self.nframes = self.tensor:size(1)
@@ -235,8 +238,6 @@ do
    -- forward
    -- a simple forward() method, that returns the next frame(s) available
    function vid:forward()
-      -- current pointer
-      self.current = self.current or 1
       -- nb channels
       local nchannels = #self
       if nchannels == 1 then
